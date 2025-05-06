3a9bfebd69c73abaa14c05023cd0469085503b26
Sakshimehra072 <sakshimehra072@gmail.com>
2025-05-06 15:49:22 +0530

    finance

---
+++ sw.js
@@ -0,0 +1,37 @@
+// sw.js - Service Worker File
+
+const CACHE_NAME = 'Pixie';
+const ASSETS_TO_CACHE = [
+    '/index.html',
+    '/about.html',
+    '/contact.html',
+    '/one-page.html',
+    '/services.html',
+    '/vendor/bootstrap/css/bootstrap.min.css',
+    '/vendor/bootstrap/css/bootstrap.css',
+    '/vendor/bootstrap/css/bootstrap.css.map',
+    '/vendor/bootstrap/css/bootstrap.min.css.map',
+  ];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
